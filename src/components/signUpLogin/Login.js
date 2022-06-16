737ebe6047c5b7813e2f0627c7ce0b741b64ac50
Shubham Arawkar <shubham.arawkar@smallcase.com>
2022-06-17 02:15:55 +0530

    refactor: login to use blueprint.js

--- src/components/signUpLogin/Login.js
+++ src/components/signUpLogin/Login.js
@@ -1,3 +1,13 @@
+import {
+  Card,
+  Button,
+  Elevation,
+  Icon,
+  Divider,
+  Intent,
+  Colors,
+  H3,
+} from "@blueprintjs/core";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,7 +20,48 @@ import {
   setTaxSetupForm,
 } from "../../actions/registerForm";
 import { getRegisterFormData } from "../../services/user.services";
+import FormInput from "./FormInput";
 
+const LOGIN_CARD_STYLING = {
+  width: "20vw",
+  minWidth: "500px",
+  marginRight: "auto",
+  marginLeft: "auto",
+  marginTop: "20vh",
+};
+
+const LOGIN_CONTAINER_STYLING = {
+  textAlign: "center",
+  padding: "2em",
+};
+
+const INPUT_CONTAINER_STYLING = {
+  padding: "2em",
+  paddingBottom: "0",
+};
+
+const DIVIDER_STYLING = {
+  marginTop: "3em",
+};
+
+const SPACE_REDUCER = () => <div style={{ marginTop: "-10px" }}></div>;
+
+function ErrorDialog({ message, success }) {
+  if (message) {
+    return (
+      <div className="form-group">
+        <br />
+        <div
+          className={success ? "alert alert-success" : "alert alert-danger"}
+          role="alert"
+        >
+          {message}
+        </div>
+      </div>
+    );
+  }
+  return <></>;
+}
 export const Login = () => {
   const [successful, setSuccessful] = useState(true);
   const dispatch = useDispatch();
@@ -21,9 +72,10 @@ export const Login = () => {
   const {
     register,
     handleSubmit,
-    // watch,
-    // formState: { errors },
+    formState: { errors },
   } = useForm();
+
+  console.log({ errors });
   const onSubmit = (data) => {
     const { email: username, password } = data;
 
@@ -91,33 +143,68 @@ export const Login = () => {
 
   return (
     <div>
-      <h1>Login</h1>
-
       <form onSubmit={handleSubmit(onSubmit)}>
+        <Card
+          style={LOGIN_CARD_STYLING}
+          interactive={true}
+          elevation={Elevation.TWO}
+        >
+          <div style={LOGIN_CONTAINER_STYLING}>
+            <Icon icon="user" size={"8em"} color={Colors.LIGHT_GRAY1}/>
+            <Divider style={DIVIDER_STYLING} />
+            <div style={INPUT_CONTAINER_STYLING}>
+              <FormInput
+                register={register}
+                validations={{
+                  required: true,
+                  minLength: 1,
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  },
+                }}
+                errors={errors}
+                field={"email"}
+                icon={"envelope"}
+                inputProps={{
+                  large: true,
+                  type: "email",
+                  subLabel: "",
+                  placeholder: "Please enter your email",
+                  errorMessage: "Enter a valid email address",
+                }}
+              />
+              <SPACE_REDUCER />
+              <FormInput
+                register={register}
+                icon={"shield"}
+                validations={{
+                  required: true,
+                  minLength: 8,
+                }}
+                errors={errors}
+                field={"password"}
+                inputProps={{
+                  large: true,
+                  type: "password",
+                  placeholder: "Enter your password",
+                  errorMessage: "Password cannot be less than 8 characters",
+                }}
+              />
+              <Button type="submit" large intent={Intent.PRIMARY}>
+                Sign In
+              </Button>
+
+              <ErrorDialog message={message} success={successful} />
+            </div>
+          </div>
+        </Card>
+
         {/* register your input into the hook by invoking the "register" function */}
-        <label>Email</label>
-        <input {...register("email")} />
-        <label>Password</label>
-        <input type="password" {...register("password")} />
 
         {/* include validation with required or other standard HTML validation rules */}
         {/* errors will return when field validation fails  */}
         {/* {errors.exampleRequired && <p>This field is required</p>} */}
-
-        <input type="submit" />
       </form>
-      {message && (
-        <div className="form-group">
-          <div
-            className={
-              successful ? "alert alert-success" : "alert alert-danger"
-            }
-            role="alert"
-          >
-            {message}
-          </div>
-        </div>
-      )}
     </div>
   );
 };
