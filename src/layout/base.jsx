e2c4d3a53d6ec91235a0c849b6f2579da13887b8
Shubham Arawkar <shubham.arawkar@smallcase.com>
2022-07-04 02:40:26 +0530

    add: `BaseLayout` component

--- src/layout/base.jsx
+++ src/layout/base.jsx
@@ -1,10 +1,36 @@
 import AppNavigation from "./navbar";
+import { AppSidebar } from "./sidebar";
 
+import { THEME } from "../theme/base";
+import { useLocation } from "react-router-dom";
+
+const { sidebar, navbar } = THEME;
+const NON_LAYOUT_ROUTES = [
+  "/",
+  "/sign-up",
+  "/confirm-sign-up",
+  "/login",
+  "/forgot-password",
+  "/confirm-forgot-password"
+];
 export default function BaseLayout(props) {
+  const location = useLocation();
+  console.log(location.pathname, { maxHeight: `calc(100vh - ${navbar.height})` });
+  if (NON_LAYOUT_ROUTES.includes(location.pathname))
+    return (
+      <>
+        {props.children}
+      </>
+    );
   return (
-    <>
-      <AppNavigation />
-      {props.children}
-    </>
+    <div>
+      <AppSidebar />
+      <div style={{ paddingLeft: sidebar.width, paddingTop: navbar.height }}>
+        <AppNavigation sidebarWidth={sidebar.width} />
+        <div style={{ maxHeight: "calc(100vh-4em)" }}>
+          {props.children}
+        </div>
+      </div>
+    </div>
   );
 }
