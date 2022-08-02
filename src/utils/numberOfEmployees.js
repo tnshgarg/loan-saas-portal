ba1dfd3d358431899c3d9b4f03b4b1afd8e8325f
supatomic <kngoyal.cs@gmail.com>
2022-08-02 18:26:24 -0400

    make styling changes to signup login pages and hook to new cognito pool

--- src/utils/numberOfEmployees.js
+++ src/utils/numberOfEmployees.js
@@ -2,28 +2,28 @@
 
 export const numberOfEmployees = [
   {
-    value: "50",
+    value: "<50",
     label: "<50",
   },
   {
-    value: "100",
+    value: "51-100",
     label: "51-100",
   },
 
   {
-    value: "500",
+    value: "101-500",
     label: "101-500",
   },
   {
-    value: "750",
+    value: "501-1000",
     label: "501-1000",
   },
   {
-    value: "900",
+    value: "1001-10000",
     label: "1001-10000",
   },
   {
-    value: "1000",
+    value: ">10000",
     label: ">10000",
   },
 ];
