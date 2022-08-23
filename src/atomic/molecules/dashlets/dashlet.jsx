d1dc1e002b1182d5d95a1362c25f1522fc673313
shubham <arawkar.shubham08@gmail.com>
2022-08-23 14:54:30 +0530

    add: `Dashlet` Component for admin panels

---
+++ src/atomic/molecules/dashlets/dashlet.jsx
@@ -0,0 +1,30 @@
+import styles from "./dashlet.module.css";
+import { Card, Divider, Elevation, H3, Icon } from "@blueprintjs/core";
+
+export const CARD_STYLING = {
+  marginLeft: "2.7em",
+  marginRight: "2.7em",
+};
+
+export const HEADER_CLASS = `${styles.column} ${styles.header}`;
+export const ACTIONS_CLASS = `${styles.column} ${styles.actions}`;
+
+export function Dashlet({ title, icon, actions, children }) {
+  return (
+    <Card style={CARD_STYLING} elevation={Elevation.THREE}>
+      <div className={styles.row}>
+        <div className={HEADER_CLASS}>
+          <H3>
+            <Icon icon={icon} size={"1em"} />
+            &nbsp;{title}
+          </H3>
+        </div>
+        <div className={ACTIONS_CLASS}>
+          <div className={styles.alignRight}>{actions}</div>
+        </div>
+      </div>
+      <Divider />
+      {children}
+    </Card>
+  );
+}
