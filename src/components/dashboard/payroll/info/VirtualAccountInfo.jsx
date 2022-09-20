d40deed6a7e830c578b212fb184474778f4a9284
shubham <arawkar.shubham08@gmail.com>
2022-09-20 16:28:40 +0530

    create: Virtual Account Info Component

---
+++ src/components/dashboard/payroll/info/VirtualAccountInfo.jsx
@@ -0,0 +1,74 @@
+import {
+  ControlGroup,
+  Intent,
+  Label,
+  Switch,
+  Tag,
+  Text,
+} from "@blueprintjs/core";
+import { useState } from "react";
+
+const Field = ({ label, contents }) => (
+  <>
+    <Label>
+      {label}
+      <Text tagName={"div"} style={{ fontSize: 20, width: "30vw" }}>
+        {contents}
+      </Text>
+    </Label>
+  </>
+);
+
+export function VirtualAccountInfo({
+  account_number,
+  account_status,
+  balance,
+  ifsc,
+  updated_at,
+}) {
+  const [mask, setMasked] = useState(true);
+  const intentMap = {
+    PENDING: Intent.WARNING,
+    ACTIVE: Intent.SUCCESS,
+  };
+  if (mask) {
+    account_number = "**********" + account_number.slice(10);
+    ifsc = "**********" + ifsc.slice(8);
+  }
+  return (
+    <div>
+      <ControlGroup fill={true}>
+        <Field label={"Bank Account Number"} contents={account_number} />
+        <Field label={"IFSC"} contents={ifsc} />
+        <Field
+          label={"Hide Details"}
+          contents={
+            <Switch
+              checked={mask}
+              onChange={() => setMasked(!mask)}
+              innerLabel={"visible"}
+              innerLabelChecked={"masked"}
+            />
+          }
+        />
+      </ControlGroup>
+      <ControlGroup fill={true}>
+        <Field label={"Balance"} contents={(balance ?? 0).toINR() + " INR"} />
+        <Field
+          label={"Updated On"}
+          contents={
+            <Tag minimal>{new Date(updated_at).toString().split("GMT")[0]}</Tag>
+          }
+        />
+        <Field
+          label={"Status"}
+          contents={
+            <Tag intent={intentMap[account_status] ?? Intent.DANGER}>
+              {account_status}
+            </Tag>
+          }
+        />
+      </ControlGroup>
+    </div>
+  );
+}
