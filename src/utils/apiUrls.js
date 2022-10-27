ff9d85966d8417994fb3f58080cbe15b27731893
prachir1501 <prachir1501@gmail.com>
2022-10-27 21:12:22 +0530

    Remove extra `employer` piece from Base API URL

--- src/utils/apiUrls.js
+++ src/utils/apiUrls.js
@@ -1,3 +1,3 @@
-export const EMPLOYER_BASE_API_URL = `https://api-${process.env.REACT_APP_STAGE}.unipe.co/employer/employer`;
+export const EMPLOYER_BASE_API_URL = `https://api-${process.env.REACT_APP_STAGE}.unipe.co/employer`;
 
 export const TIMEOUT = 5 * 60;
