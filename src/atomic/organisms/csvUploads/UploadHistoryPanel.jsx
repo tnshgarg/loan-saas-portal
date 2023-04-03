b5c0d140da3f89bc1f7eaba0a85799ac09ca5fc8
prachir1501 <prachir1501@gmail.com>
2023-04-03 17:33:56 +0530

    Add output file download button in Panel

--- src/atomic/organisms/csvUploads/UploadHistoryPanel.jsx
+++ src/atomic/organisms/csvUploads/UploadHistoryPanel.jsx
@@ -1,36 +1,44 @@
 import { Button, Intent, Spinner, Tag } from "@blueprintjs/core";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import {
-  useGetUploadedFilesQuery
-} from "../../../store/slices/apiSlices/files/filesApiSlice";
+import { useGetUploadedFilesQuery } from "../../../store/slices/apiSlices/files/filesApiSlice";
 import { initBrowserTable } from "../../../store/slices/browserTableSlice.ts";
 import { Dashlet } from "../../molecules/dashlets/dashlet";
 import BrowserTable from "../browserTable";
 import { StatusCell } from "../browserTable/cells";
 
 const DownloadFileCell = ({ value }) => (
-  <div style={{marginTop: "-6px",marginBottom: "-6px", textAlign: "center",marginLeft: "-2em",marginRight: "-2em",padding: 0}}>
-    <a
-      href={value ?? null}
-      target="_blank"
-      rel="noreferrer"
-    >
-      <Button icon={"download"} intent={Intent.PRIMARY} minimal={true}>
+  <div
+    style={{
+      marginTop: "-6px",
+      marginBottom: "-6px",
+      textAlign: "center",
+      marginLeft: "-2em",
+      marginRight: "-2em",
+      padding: 0,
+    }}
+  >
+    <a href={value ?? null} target="_blank" rel="noreferrer">
+      <Button
+        icon={"download"}
+        intent={Intent.PRIMARY}
+        minimal={true}
+        disabled={value ? false : true}
+      >
         Download
       </Button>
     </a>
   </div>
-)
+);
 
 const TABLE_COLUMNS = [
   { Header: "File Name", accessor: "inputFileName" },
   { Header: "Upload Time", accessor: "uploadedAt" },
   { Header: "Status", accessor: "inputFileStatus" },
-  { Header: "Download File", accessor: "inputFileUrl" },
-]
+  { Header: "Download Input File", accessor: "inputFileUrl" },
+  { Header: "Download Output File", accessor: "outputFileUrl" },
+];
 const UploadHistoryTable = ({ employerId, module, handlers }) => {
-
   const { data, isLoading, error, refetch } = useGetUploadedFilesQuery({
     id: employerId,
     module,
@@ -39,39 +47,41 @@ const UploadHistoryTable = ({ employerId, module, handlers }) => {
   const stateTableName = module + "-uploads-history";
   const dispatch = useDispatch();
   const updateTableRows = (body) => {
-    dispatch(initBrowserTable({
-      data: body.map((val) => Object.assign({}, val)),
-      fields: TABLE_COLUMNS,
-      fileName: stateTableName,
-      module: stateTableName,
-    }))
+    dispatch(
+      initBrowserTable({
+        data: body.map((val) => Object.assign({}, val)),
+        fields: TABLE_COLUMNS,
+        fileName: stateTableName,
+        module: stateTableName,
+      })
+    );
   };
 
   useEffect(() => {
-    console.log("data Updated 1",data)
+    console.log("data Updated 1", data);
     if (data) {
-      console.log("data Updated 2",data)
+      console.log("data Updated 2", data);
       const body = data?.body || [];
       updateTableRows(body);
     }
-    
   }, [data]);
-
 
   handlers["refresh"] = () => {
     refetch();
   };
 
-  let component = ""
-  console.log({isLoading,error})
+  let component = "";
+  console.log({ isLoading, error });
   if (isLoading) {
-    component = (<Spinner style={{ marginTop: "2em", marginBottom: "2em" }} size={54} />)
+    component = (
+      <Spinner style={{ marginTop: "2em", marginBottom: "2em" }} size={54} />
+    );
   } else if (error) {
     component = (
       <Tag icon={"error"} intent={Intent.DANGER} large minimal>
         {error}
       </Tag>
-    )
+    );
   } else {
     component = (
       <BrowserTable
@@ -80,18 +90,15 @@ const UploadHistoryTable = ({ employerId, module, handlers }) => {
         tableName={stateTableName}
         disableEdits={true}
         customCells={{
-          "inputFileStatus": StatusCell,
-          "inputFileUrl": DownloadFileCell
+          inputFileStatus: StatusCell,
+          inputFileUrl: DownloadFileCell,
+          outputFileUrl: DownloadFileCell,
         }}
       />
-    )
+    );
   }
 
-  return (
-    <>
-      {component}
-    </>
-  );
+  return <>{component}</>;
 };
 
 export const UploadHistoryPanel = ({ employerId, module }) => {
@@ -103,7 +110,7 @@ export const UploadHistoryPanel = ({ employerId, module }) => {
         title={"Uploaded Files"}
         actions={
           <>
-            <Button icon={"refresh"} onClick={() => handlers['refresh']()}>
+            <Button icon={"refresh"} onClick={() => handlers["refresh"]()}>
               Refresh
             </Button>
           </>
