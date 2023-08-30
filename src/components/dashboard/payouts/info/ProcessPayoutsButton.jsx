90fad3dc9d9cadfa0c0c5bbe626a0227de19f8bd
prachir1501 <prachir1501@gmail.com>
2023-08-30 17:55:17 +0530

    add : popover button for payouts processing

--- src/components/dashboard/payouts/info/ProcessPayoutsButton.jsx
+++ src/components/dashboard/payouts/info/ProcessPayoutsButton.jsx
@@ -1,7 +1,9 @@
-import { connect } from "react-redux";
 import { Button, Intent } from "@blueprintjs/core";
-import { FS } from "../../employee/onboarding/validations";
+import { Classes, Popover2 } from "@blueprintjs/popover2";
+import { connect } from "react-redux";
+import { Spacer } from "../../../../atomic/atoms/layouts/alignment";
 import { CSVUploadsStateMapper } from "../../../../atomic/organisms/browserTable";
+import { FS } from "../../employee/onboarding/validations";
 function _ProcessPayoutsButton({
   data,
   employerId,
@@ -26,17 +28,59 @@ function _ProcessPayoutsButton({
     payload["provider"] = provider;
     processPayouts(payload);
   };
+
+  const getPayoutsSummary = () => {
+    const selected = data.filter((item) => item.status[FS.SELECTED]);
+    const totalPayouts = selected.length;
+    const netAmount = selected.reduce((totalPayoutsAmount, currentPayout) => {
+      const currentPayoutAmount = parseInt(currentPayout.amount);
+      return totalPayoutsAmount + currentPayoutAmount;
+    }, 0);
+
+    return (
+      <>
+        <p>{`Total Payouts : ${totalPayouts}`}</p>
+        <p>{`Net Amount : ${netAmount.toINR()}`}</p>
+      </>
+    );
+  };
+
   return (
     <>
-      <Button
-        disabled={loading || disabled}
-        loading={loading}
-        icon={"bank-account"}
-        intent={Intent.PRIMARY}
-        onClick={onClick}
-      >
-        Process Payouts
-      </Button>
+      <Popover2
+        interactionKind="click"
+        popoverClassName={Classes.POPOVER2_CONTENT_SIZING}
+        placement="bottom"
+        content={
+          <div>
+            <h3>Payouts Summary</h3>
+            {getPayoutsSummary()}
+            <Button
+              className={Classes.POPOVER2_DISMISS}
+              text="Cancel"
+              intent={Intent.DANGER}
+            />
+            <Spacer />
+            <Button
+              className={Classes.POPOVER2_DISMISS}
+              text="Confirm"
+              intent={Intent.SUCCESS}
+              onClick={onClick}
+            />
+          </div>
+        }
+        renderTarget={({ isOpen, ref, ...targetProps }) => (
+          <Button
+            {...targetProps}
+            elementRef={ref}
+            disabled={loading || disabled}
+            loading={loading}
+            icon={"bank-account"}
+            intent={Intent.PRIMARY}
+            text="Process Payouts"
+          />
+        )}
+      />
     </>
   );
 }
